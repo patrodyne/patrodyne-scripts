90824abedfc073457e526d7e142f07ea6df4358b
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2022-06-09 12:02:41 -0400

    Added Variable Bit Rate (VBR) version.

---
+++ flac2mp3/flac2mp3-vbr.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+# PatroDyne: Patron Supported Dynamic Executables, http://patrodyne.org
+# MIT license: https://raw.githubusercontent.com/patrodyne/patrodyne-scripts/master/LICENSE
+#
+# flac2mp3-vbr.sh - Linux script to convert FLAC audio files to MP3 files using VBR.
+#
+# Usage:
+#
+#   Background: ./flac2mp3-vbr.sh </dev/null >flac2mp3-vbr.log 2>&1 &
+#   Foreground: ./flac2mp3-vbr.sh 2>&1 | tee flac2mp3-vbr.log
+#
+# Description: This script recursively finds all files with '*.flac' suffix
+# within the SOURCEDIR, it uses ffmpeg to convert each file and write the
+# '*.mp3' transformation into the TARGETDIR. Hidden files are ignored. Target
+# folders are created, as needed.  Files are processed in groups of parallel
+# background jobs. The group size is set using the CORES variable. Set CORES to
+# the number of CPU core(s) that you have (or less to reserve CPU(s) for other
+# work). If the source directories contain a COVERART image file, it will be
+# embedded in each associated target file.
+#
+# Set the SOURCEDIR, TARGETDIR, COVERART, VBRATE and CORES per your requirements.
+#
+
+SOURCEDIR="PATH_TO_FLAC/flac"
+TARGETDIR="PATH_TO_MP3/mp3"
+COVERART="cover.jpg"
+VBRATE="4" # 0-9: 0 is highest quality.
+CORES=4
+
+BASEDIR="$(dirname $0)"
+SOURCEFMT="flac"
+TARGETFMT="mp3"
+COUNTER=0
+INDEX=0
+
+DONE=false
+find ${BASEDIR}/${SOURCEDIR} -name '*' | until ${DONE}
+  do
+    read SOURCE || DONE=true
+    # Copy non-FLACs as is, insert FLACs to process in array
+    if [[ ! "${SOURCE}" =~ .*/\..* ]]; then
+      TARGET=$(echo "${SOURCE}" | sed -e "s#^${BASEDIR}/${SOURCEDIR}#${BASEDIR}/${TARGETDIR}#")
+      if [[ -d "${SOURCE}" && ! -e "${TARGET}" ]]; then
+        mkdir -p "${TARGET}"
+      elif [[ -f "${SOURCE}" && ! -e "${TARGET}" ]]; then
+        if [[ "${SOURCE}" =~ .*\.${SOURCEFMT} ]]; then
+          COUNTER=$(expr ${COUNTER} + 1)
+          INDEX=$(expr ${INDEX} + 1)
+          SOURCE[$INDEX]="${SOURCE}"
+          TARGET[$INDEX]="${TARGET%.*}.${TARGETFMT}"
+        else
+          cp "${SOURCE}" "${TARGET}"
+        fi
+      fi
+    fi
+
+    # Process multiple FLACs in parallel.
+    if [[ "${DONE}" = "true" || $(expr ${COUNTER} % ${CORES}) -eq 0 ]]; then
+      while [ ${INDEX} -gt 0 ]
+        do
+          SOURCEART=$(dirname "${SOURCE[$INDEX]}")/${COVERART}
+          if [ -f "${SOURCEART}" ]; then
+            echo "${COUNTER}.${INDEX}, COVER: ${SOURCE[$INDEX]}"
+            ffmpeg -nostdin -loglevel error -i "${SOURCE[$INDEX]}" \
+              -i "${SOURCEART}" -map 0:0 -map 1:0 \
+              -metadata:s:v title="Album cover" \
+              -metadata:s:v comment="Cover (front)" \
+              -map_metadata 0 -codec:a libmp3lame -q:a ${VBRATE} "${TARGET[$INDEX]}" &
+            PID[INDEX]=$!
+          else
+            echo "${COUNTER}.${INDEX}, NOCOVER: ${SOURCE[$INDEX]}"
+            ffmpeg -nostdin -loglevel error -i "${SOURCE[$INDEX]}" \
+              -map_metadata 0 -codec:a libmp3lame -q:a ${VBRATE} "${TARGET[$INDEX]}" &
+            PID[INDEX]=$!
+          fi
+          INDEX=$(expr ${INDEX} - 1)
+        done
+      wait $(printf "%s " "${PID[@]}") >/dev/null 2>&1
+    fi
+  done
